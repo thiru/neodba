1785760ca26b946cd1a7201a12de80bc1cc26ee1
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-27 20:46:08 -0400

    Simplify result set printing

--- src/neodba/dba.clj
+++ src/neodba/dba.clj
@@ -4,6 +4,7 @@
     [clojure.edn :as edn]
     [clojure.pprint :as pp]
     [next.jdbc :as jdbc]
+    [next.jdbc.result-set :as jdbc-rs]
     [neodba.utils.common :as u]))
 
 
@@ -17,11 +18,12 @@
 (defn execute [sql]
   (let [db (get-connection-map)]
     (with-open [con (jdbc/get-connection db)]
-      (jdbc/execute! con [sql]))))
+      (jdbc/execute! con
+                     [sql]
+                     {:builder-fn jdbc-rs/as-unqualified-lower-maps}))))
 
 (defn print-query-res [query-res]
-  (let [sans-ns (map #(update-keys % name) query-res)]
-    (pp/print-table sans-ns)))
+  (pp/print-table query-res))
 
 (comment
   (print-query-res (execute "select * from users")))
